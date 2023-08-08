cfacbd133bb5452e98cc6b94efd42c6f092547e2
Daniel Flores <danielfg240206@gmail.com>
2023-08-08 01:40:37 -0600

    Version1.1.0 del programa

--- Juego3enRaya.py
+++ Juego3enRaya.py
@@ -1,56 +1,51 @@
 import tkinter as tk
 
 # Configuración de colores
-COLOR_FONDO = "#00387f"        # Azul oscuro como fondo
-COLOR_BOTON = "#00cfff"        # Azul claro para los botones
-COLOR_TEXTO = "#005cd5"        # Azul para el texto principal
-COLOR_TEXTO_BOTON = "#FFFFFF"  # Texto blanco para los botones
-COLOR_TEXTO_GANADOR = "#00fed9"  # Turquesa claro para el texto del ganador
-COLOR_ETIQUETA = COLOR_BOTON   # Misma etiqueta que el color de botones
-COLOR_CAJA_TEXTO = "#9bffad"   # Verde claro para las cajas de texto
+COLOR_FONDO = "#00387f"
+COLOR_BOTON = "#00cfff"
+COLOR_TEXTO = "#005cd5"
+COLOR_TEXTO_BOTON = "#FFFFFF"
+COLOR_TEXTO_GANADOR = "#00fed9"
+COLOR_ETIQUETA = COLOR_BOTON
+COLOR_CAJA_TEXTO = "#9bffad"
 
 ventanaRegistro = tk.Tk()
 ventanaRegistro.geometry("400x200")
 ventanaRegistro.title("Ingresa los nombres de usuario")
 ventanaRegistro.configure(bg=COLOR_FONDO)
 
-# Lista para almacenar las etiquetas de error
-etiquetasFaltaUsuarios = [] 
+etiquetasFaltaUsuarios = []
 
 def VentanaDeUsuarios():
     global etiqueta1, etiqueta2, cajaDeTexto1, cajaDeTexto2
 
-    # Etiqueta y caja para el usuario 1
     etiqueta1 = tk.Label(ventanaRegistro, text="Ingrese el nombre del usuario 1:", bg=COLOR_FONDO, fg=COLOR_TEXTO_BOTON)
     etiqueta1.pack()
-    
+
     cajaDeTexto1 = tk.Entry(ventanaRegistro, font="Helvetica 10", bg=COLOR_CAJA_TEXTO, fg=COLOR_TEXTO)
     cajaDeTexto1.pack()
 
-    # Etiqueta y caja para el usuario 2
     etiqueta2 = tk.Label(ventanaRegistro, text="Ingrese el nombre del usuario 2:", bg=COLOR_FONDO, fg=COLOR_TEXTO_BOTON)
     etiqueta2.pack()
-    
+
     cajaDeTexto2 = tk.Entry(ventanaRegistro, font="Helvetica 10", bg=COLOR_CAJA_TEXTO, fg=COLOR_TEXTO)
     cajaDeTexto2.pack()
 
 def TextoDeLaCaja():
     global usuario1, usuario2
 
-    # Obtener los nombres de usuario de las cajas de texto
     usuario1 = cajaDeTexto1.get()
     usuario2 = cajaDeTexto2.get()
 
-    # Limpiar etiquetas de error anteriores
     for etiqueta in etiquetasFaltaUsuarios:
         etiqueta.destroy()
     etiquetasFaltaUsuarios.clear()
 
-    # Comprobar si los nombres de usuario se han ingresado
     if usuario1 != "" and usuario2 != "":
         ventanaRegistro.destroy()
         """------------------------------------------------------------------------------------------------------------------------------------------------------------"""
-        # Agregar elementos a la ventana de juego
+
+                            # Agregar elementos a la ventana de juego
         ventanaJuego = tk.Tk()
         ventanaJuego.geometry("400x400")
         ventanaJuego.title("Ventana de Juego")
@@ -82,9 +77,13 @@ def TextoDeLaCaja():
 
         class JuegoGato():
             global ganador
+            
             def __init__(self):
                 self.turno = 1
                 self.ganador = None
+                self.victorias_jugador1 = 0
+                self.victorias_jugador2 = 0
+                self.empate = 0
                 
             def turnoJuego(self, indice):
                 if self.turno % 2 != 0:
@@ -99,56 +98,42 @@ def TextoDeLaCaja():
                     else:
                         botones[indice]["text"] = "O"
                         self.turno += 1
-                if self.turno >= 9:
-                    ganador = "El juego a quedado en empate"
-                    print(ganador)
-                    VentanaDeFinal(ganador)
+                
                 self.verificarGanador()
 
             def verificarGanador(self):
                 ganador = ""
-                # Implementa la lógica para verificar si hay un ganador
-                #Checa si gano el primer Jugador
-                if botones[0]["text"] == "X":
-                    if botones[1]["text"] == "X" and botones[2]["text"] == "X":
-                        ganador = usuario1
-                    if botones[3]["text"] == "X" and botones[6]["text"] == "X":
-                        ganador = usuario1
-                    if botones[4]["text"] == "X" and botones[8]["text"] == "X":
-                        ganador = usuario1
-                if botones[1]["text"] == "X" and botones[4]["text"] == "X" and botones[7]["text"] == "X":
-                   ganador = usuario1
-                if botones[2]["text"] == "X":
-                    if botones[4]["text"] == "X" and botones[6]["text"] == "X":
-                        ganador = usuario1
-                    if botones[5]["text"] == "X" and botones[8]["text"] == "X":
-                        ganador = usuario1
-                if botones[3]["text"] == "X" and botones[4]["text"] == "X" and botones[5]["text"] == "X":
-                   ganador = usuario1
-                if botones[6]["text"] == "X" and botones[7]["text"] == "X" and botones[8]["text"] == "X":   
-                   ganador = usuario1
+    
+                # Filas
+                for i in range(0, 9, 3):
+                    if botones[i]["text"] == botones[i + 1]["text"] == botones[i + 2]["text"] != " ":
+                        ganador = botones[i]["text"]
+                        
+                # Columnas
+                for i in range(3):
+                    if botones[i]["text"] == botones[i + 3]["text"] == botones[i + 6]["text"] != " ":
+                        ganador = botones[i]["text"]
 
-                #Checa si gano el primer Jugador
-                if botones[0]["text"] == "O":
-                    if botones[1]["text"] == "O" and botones[2]["text"] == "O":
-                        ganador = usuario2
-                    if botones[3]["text"] == "O" and botones[6]["text"] == "O":
-                        ganador = usuario2
-                    if botones[4]["text"] == "O" and botones[8]["text"] == "O":
-                        ganador = usuario2
-                if botones[1]["text"] == "O" and botones[4]["text"] == "O" and botones[7]["text"] == "O":
-                   ganador = usuario2
-                if botones[2]["text"] == "O":
-                    if botones[4]["text"] == "O" and botones[6]["text"] == "O":
-                        ganador = usuario2
-                    if botones[5]["text"] == "O" and botones[8]["text"] == "O":
-                        ganador = usuario2
-                if botones[3]["text"] == "O" and botones[4]["text"] == "O" and botones[5]["text"] == "O":
-                   ganador = usuario2
-                if botones[6]["text"] == "O" and botones[7]["text"] == "O" and botones[8]["text"] == "O":  
-                   ganador = usuario2  
-                
-                if ganador == usuario1 or ganador == usuario2:
+                # Diagonales
+                if botones[0]["text"] == botones[4]["text"] == botones[8]["text"] != " ":
+                    ganador = botones[0]["text"]
+                elif botones[2]["text"] == botones[4]["text"] == botones[6]["text"] != " ":
+                    ganador = botones[2]["text"]
+
+                 #Puntuaciones
+                if ganador == usuario1:
+                    juego.victorias_jugador1 += 1
+                elif ganador == usuario2:
+                    juego.victorias_jugador2 += 1
+
+                #Comprueba si alguien ya gano
+                if ganador != "":
+                    VentanaDeFinal(ganador)
+
+                #Si nadie gano
+                if self.turno >= 10 and ganador == "":
+                    ganador = "El juego a quedado en empate"
+                    self.empate += 1
                     VentanaDeFinal(ganador)
 
 
@@ -166,12 +151,11 @@ def TextoDeLaCaja():
             ventanaFinal.title("Ventana Final")
             ventanaFinal.configure(bg=COLOR_FONDO)
             ventanaJuego.withdraw()  # Ocultar ventanaJuego
-            if ganador == usuario1:
-                textoFinal = f"Gano {usuario1}"
-            elif ganador == usuario2:
-                textoFinal = f"Gano {usuario2}"
-            else:
+            #Adjunta a la variable textoFinal el texto a mostrar en pantalla 
+            if ganador == "El juego a quedado en empate":
                 textoFinal = ganador
+            else:
+                textoFinal = f"{ganador} a ganado"
             etiquetaFinal = tk.Label(ventanaFinal, text=textoFinal, bg=COLOR_ETIQUETA, fg=COLOR_TEXTO_BOTON)
             etiquetaFinal.pack()
             etiquetaVolveraJugar = tk.Label(ventanaFinal, text="Quieren volver a jugar?", bg=COLOR_FONDO, fg=COLOR_TEXTO_BOTON)
@@ -202,30 +186,25 @@ def TextoDeLaCaja():
         ventanaJuego.mainloop()
 
         """---------------------------------------------------------------------------------------------------------------------------------------------------------------"""
+
     elif usuario1 == "" and usuario2 != "":
-        # Etiqueta de error si falta el usuario 1
-        etiquetaFaltaUsuarios = tk.Label(ventanaRegistro, text="Ingrese el nombre del usuario 1 ")
+        etiquetaFaltaUsuarios = tk.Label(ventanaRegistro, text="Ingrese el nombre del usuario 1")
         etiquetaFaltaUsuarios.pack()
         etiquetasFaltaUsuarios.append(etiquetaFaltaUsuarios)
     elif usuario1 != "" and usuario2 == "":
-        # Etiqueta de error si falta el usuario 2
-        etiquetaFaltaUsuarios = tk.Label(ventanaRegistro, text="Ingrese el nombre del usuario 2 ")
+        etiquetaFaltaUsuarios = tk.Label(ventanaRegistro, text="Ingrese el nombre del usuario 2")
         etiquetaFaltaUsuarios.pack()
         etiquetasFaltaUsuarios.append(etiquetaFaltaUsuarios)
     else:
-        # Etiqueta de error si faltan ambos nombres
-        etiquetaFaltaUsuarios = tk.Label(ventanaRegistro, text="Ingrese ambos nombres ")
+        etiquetaFaltaUsuarios = tk.Label(ventanaRegistro, text="Ingrese ambos nombres")
         etiquetaFaltaUsuarios.pack()
         etiquetasFaltaUsuarios.append(etiquetaFaltaUsuarios)
 
-# Crear la interfaz de la ventana de registro
 VentanaDeUsuarios()
 
-# Botón para procesar los nombres de usuario
-boton = tk.Button(ventanaRegistro, text="Ingresar", command=TextoDeLaCaja,bg=COLOR_BOTON, fg=COLOR_TEXTO_BOTON)
+boton = tk.Button(ventanaRegistro, text="Ingresar", command=TextoDeLaCaja, bg=COLOR_BOTON, fg=COLOR_TEXTO_BOTON)
 boton.pack()
 
-# Iniciar el bucle de la ventana principal
 ventanaRegistro.mainloop()
 
 
